97dcbb586164772116901add9d12e077d92f8e13
Lluís Revilla <lluis.revilla@gmail.com>
2017-01-13 16:03:03 +0100

    Adapt the test to the split of weighted

--- tests/testthat/test_auxiliar.R
+++ tests/testthat/test_auxiliar.R
@@ -30,19 +30,32 @@ test_that("combinadic", {
 })
 
 # weighted ####
-test_that("weighted", {
+test_that("weighted.sum", {
     set.seed(5)
     x <- runif(5)
     weights <-  c(0.1, 0.2, 0.3, 0.4, 0.0)
-    test <- weighted(x, weights)
+    test <- weighted.sum(x, weights)
     expect_equal(test, 0.54588768)
-    expect_error(weighted(x, c(0.1, 0.2)), "match the length")
-    expect_error(weighted("a", c(0.1)), "should be numeric")
-    expect_error(weighted(0.1, "a"), "should be numeric")
+    expect_error(weighted.sum(x, c(0.1, 0.2)), "match the length")
+    expect_error(weighted.sum("a", c(0.1)), "should be numeric")
+    expect_error(weighted.sum(0.1, "a"), "should be numeric")
     weights <-  c(0.1, 0.2, 0.3, 0.4, 0.2)
-    expect_warning(weighted(x, weights), "the weights is above 1")
+    expect_warning(weighted.sum(x, weights), "the weights is above 1")
 })
 
+test_that("weighted.prod", {
+    set.seed(5)
+    x <- runif(5)
+    weights <-  c(0.1, 0.2, 0.3, 0.4, NA)
+    test <- weighted.prod(x, weights)
+    expect_equal(weighted.prod(x, c(0.1, 0.2, 0.3, 0.4, 0)), 0L)
+    expect_equal(test, 8.58568731153938e-05)
+    expect_error(weighted.prod(x, c(0.1, 0.2)), "match the length")
+    expect_error(weighted.prod("a", c(0.1)), "should be numeric")
+    expect_error(weighted.prod(0.1, "a"), "should be numeric")
+    weights <-  c(0.1, 0.2, 0.3, 0.4, 0.2)
+    expect_warning(weighted.prod(x, weights), "the weights is above 1")
+})
 # duplicateIndices ####
 test_that("duplicateIndices", {
     vec <- c("52", "52", "52", "53", "55")
