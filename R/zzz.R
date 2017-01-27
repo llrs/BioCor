b7e98a3c68edde00f881d17356f00f261a76d80f
Lluís Revilla <lluis.revilla@gmail.com>
2017-01-27 16:32:28 +0100

    Moving issues tracker to the Description, and adding webpage of the package

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,8 @@
 ##' @importFrom utils packageDescription
 .onAttach <- function(libname, pkgname) {
-
-    pkgVersion <- packageDescription(pkgname, fields = "Version")
-    msg <- paste0(pkgname, " v", pkgVersion, "  ",
-                  "For issues: https://github.com/llrs/", pkgname, "\n\n")
-
     citation <- paste0("If you use ", pkgname,
                        " in published research, please cite:\n")
 
-    packageStartupMessage(msg, citation)
+    packageStartupMessage(citation)
     invisible()
 }
