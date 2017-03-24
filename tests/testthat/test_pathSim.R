247c4ad49526e423e057442b007a261b953f08df
Lluís Revilla <lluis.revilla@gmail.com>
2017-03-24 16:59:47 +0100

    Test pathSims_matrix

--- tests/testthat/test_pathSim.R
+++ tests/testthat/test_pathSim.R
@@ -10,7 +10,7 @@ test_that("pathSim", {
 })
 
 test_that("mpathSim", {
-    test <- mpathSim(c("194840", "156580"), info)
+    test <- mpathSim(c("194840", "156580"), info, method = "max")
     expect_equal(test, 1L)
     test <- mpathSim(c("194840", "156580"), info, NULL)
     expect_equal(test[2L, 1L], 0.4)
@@ -18,4 +18,14 @@ test_that("mpathSim", {
     expect_error(mpathSim(1, "156580", info))
     expect_error(mpathSim("a", "156580", "a"))
     expect_error(mpathSim("a", "156580", info))
+})
+
+test_that("pathSims_matrix", {
+    test <- pathSims_matrix(info)
+    pathways <- unique(unlist(info))
+    nPathways <- length(pathways[!is.na(pathways)])
+    expect_equal(dim(test), c(nPathways, nPathways))
+    expect_true(all(diag(test) == 1L))
+    eq <- pathSim("1430728", "156580", info)
+    expect_equal(test["1430728", "156580"], eq)
 })
