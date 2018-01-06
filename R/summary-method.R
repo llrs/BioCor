ea3215b41e9612ffa4e7732a6d78eab81e060ee7
Lluís Revilla <lluis.revilla@gmail.com>
2018-01-06 12:42:24 +0100

    Use function h_index. Improve redaction of summary, remove standardGeneric

--- R/summary-method.R
+++ R/summary-method.R
@@ -1,24 +1,16 @@
 #' @importClassesFrom GSEABase GeneSetCollection
 NULL
 
-
-#' @exportMethod summary
-setGeneric("summary", function(object) {
-    standardGeneric("summary")
-})
-
 #' @exportMethod summary
 setMethod("summary",
           "GeneSetCollection",
           function(object) {
+              check_gsc(object)
               gpp <- GenesPerPathway(object)
               ppg <- PathwaysPerGene(object)
 
-              tab_gpp <- table(gpp)
-              tab_ppg <- table(ppg)
-
-              h_gpp <- tab_gpp[as.numeric(names(tab_gpp)) == tab_gpp]
-              h_ppg <- tab_ppg[as.numeric(names(tab_ppg)) == tab_ppg]
+              h_gpp <- h_index(gpp)
+              h_ppg <- h_index(ppg)
 
               out <- c(ICgpp = IC(gpp),
                        ICppg = IC(ppg),
@@ -31,12 +23,12 @@ setMethod("summary",
               )
               cat("Genes:", out["genes"], "\n")
               cat("\tGene in more pathways:", out["maxPPG"], "pathways\n")
-              cat("\th-index:", h_ppg, "genes with", h_ppg, "pathways\n")
+              cat("\th-index:", h_gpp, "genes with at least", h_gpp, "pathways.\n")
               cat("Pathways:", out["pathways"], "\n")
               cat("\tBiggest pathway:", out["maxGPP"], "genes\n")
-              cat("\th-index:", h_gpp, "pathways with", h_gpp, "genes.\n")
-              cat("IC(GenesPerPathway):", round(out["ICgpp"], 2), "close to the maximum by", round(out["ICgpp"]/out["maxICgpp"], 2), "\n")
-              cat("IC(PathwaysPerGene)", round(out["ICppg"], 2), "close to the maximum by", round(out["ICppg"]/out["maxICppg"], 2), "\n")
+              cat("\th-index:", h_ppg, "pathways with at least", h_ppg, "genes.\n")
+              cat("IC(GenesPerPathway):", round(out["ICgpp"], 2), "(", round(out["ICgpp"]/out["maxICgpp"], 2), "% of the maximum)\n")
+              cat("IC(PathwaysPerGene)", round(out["ICppg"], 2), "(", round(out["ICppg"]/out["maxICppg"], 2), "% of the maximum)\n")
 
             invisible(out)
           }
