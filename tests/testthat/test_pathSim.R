79885a2cbf9faa7e49b2326193ae6ba768aea86b
Lluís Revilla <lluis.revilla@gmail.com>
2019-01-13 14:30:43 +0100

    Test pathStim with a GeneSetCollection

--- tests/testthat/test_pathSim.R
+++ tests/testthat/test_pathSim.R
@@ -11,3 +11,14 @@ test_that("pathSim", {
     expect_equal(pathSim("109582", "114608", info), 1)
 
 })
+
+test_that("pathSim with GSC", {
+    test <- pathSim("194840", "156580", Info)
+    expect_equal(test, 0.4)
+    expect_error(pathSim(NULL, "156580", Info), "several")
+    expect_error(pathSim(1, "156580", Info), "characters")
+    expect_error(pathSim("1", "156580", "a"), "list")
+    expect_true(is.na(pathSim("1", "156580", Info)))
+    expect_true(is.na(pathSim("109582", "114608", Info)))
+
+})
