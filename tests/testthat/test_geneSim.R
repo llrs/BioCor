cdab8b60f1869416c2f8059e6ace529b3cbffe10
Lluís Revilla <lluis.revilla@gmail.com>
2019-01-13 14:28:08 +0100

    Add test with GSC
    I don't know how are they loaded

--- tests/testthat/test_geneSim.R
+++ tests/testthat/test_geneSim.R
@@ -39,3 +39,40 @@ test_that("mgeneSim", {
     expect_warning(mgeneSim(as.character(1:10), info, NULL), "be null")
     expect_error
 })
+
+test_that("geneSim with GSC", {
+
+    test <- geneSim("2", "9", Info)
+    test2 <- geneSim("9", "2", Info)
+    expect_error(geneSim(2, "9", Info), "character")
+    expect_equal(test, 0.4)
+    expect_equal(test, test2)
+    test3 <- geneSim("9", "2", info)
+    expect_equal(test2, test3)
+
+    test <- geneSim("2", "9", Info, NULL)
+    expect_true(is.matrix(test))
+    expect_false(is.null(colnames(test)))
+    expect_false(is.null(rownames(test)))
+    expect_equal(ncol(test), 4L)
+    expect_error(geneSim(c("2", "9"), "9", Info), "just one gene")
+    expect_true(is.na(geneSim("11", "12", Info)))
+    expect_error(geneSim("2", "9", as.environment(Info)), " character vector")
+    expect_true(is.na(geneSim("7", "8", Info)))
+})
+
+test_that("mgeneSim with GSC", {
+    test <- mgeneSim(c("10", "2", "3", "9", "1"), Info)
+    expect_true(is.na(test))
+
+    test <- mgeneSim(c("10", "2", "3", "9"), Info)
+    expect_true(isSymmetric(test))
+    expect_equal(test["2", "10"], 0.4)
+    expect_error(mgeneSim(c("2", "2"), Info), "geneSim")
+    test2 <- mgeneSim(c("7", "8"), Info)
+    expect_true(is.na(test2))
+    test3 <- mgeneSim(c("a" = "2", "b" = "9"), Info)
+    expect_equal(colnames(test3), letters[1:2])
+    expect_equal(rownames(test3), letters[1:2])
+    expect_error(mgeneSim(c(2, 3), Info), "character")
+})
