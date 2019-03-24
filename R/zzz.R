d2c6673a62cc4d63286ceff298b8933214bb39d0
Lluís Revilla <lluis.revilla@gmail.com>
2019-03-24 13:18:25 +0100

    Remove message at the beginning (closes #23)

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-    citation <- paste0("If you use ", pkgname,
-                       " in published research, please cite:")
-
-    packageStartupMessage(citation)
-    invisible()
-}
