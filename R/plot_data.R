6f5df7264cfb7a163d67761fe84aa24d2d837775
Lluís Revilla <lrevilla@irsicaixa.es>
2023-03-17 15:29:23 +0100

    Document how the data_plot would work.

---
+++ R/plot_data.R
@@ -0,0 +1,17 @@
+
+# Function to make it easier to plot the data:
+
+# Pick up the edges to show
+# 1: From a similarity matrix calculate the density of the edges.
+# 2: From there calculate the ecdf to pick the top x% (parameter)
+# 3: Transform it into a data.frame
+
+# Pick the distribution of the points
+# 1: calculate the dissimlarity
+# 2: calculate the PCA of the first 2 dimensions
+
+
+# Plot the entities and its edges
+
+
+# Function to plot the data with ggplot2 if installed.
