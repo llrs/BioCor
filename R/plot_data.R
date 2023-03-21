4c8eb33c516d86c56b20db5b4c4b8825ee0c033a
Lluís Revilla <lrevilla@irsicaixa.es>
2023-03-21 15:08:31 +0100

    Use plain ggplot2 for the plots

--- R/plot_data.R
+++ R/plot_data.R
@@ -1,6 +1,16 @@
 
 # Function to make it easier to plot the data:
+#' The position of the nodes is based on the similarity between them.
+#' @param x Matrix with the similarities.
+#'
+#' @param top  a number between 0 and 1 to select the edges relating the elements of the matrix.
+#' @returns A list with two elements:
+#' - nodes: The position and name of the nodes
+#' - edges: The information about the selected edges
+#' @export
+#' @rdname plot_similarity
 plot_data <- function(x, top) {
+  
   # Pick up the edges to show
   # 1: From a similarity matrix calculate the density of the edges.
   # 2: From there calculate the quantile to pick the top x% (parameter)
@@ -21,35 +31,52 @@ plot_data <- function(x, top) {
   MDS <- as.data.frame(cmdscale(1 - x))
   colnames(MDS) <- c("x", "y")
   MDS$node <- rownames(MDS)
-  pd <- list(node = MDS, edges = df)
   
+  # Prepare for plotting data
+  A <- merge(df, MDS, by.x = "A", by.y = "node")
+  B <- merge(df, MDS, by.x = "B", by.y = "node")
+  m <- merge(A, B, by = c("B", "A", "strength", "rank"), suffixes = c(".start", ".end"))
   
-  if (!requireNamespace("ggraph", quietly = TRUE)) {
-    warning("tidygraph is not present: you'll need to work out how to represent the output")
-    return(pd)
-  }
-  
-  edges <- pd$edges
-  colnames(edges)[1:2] <- c("from", "to")
-  node <- pd$node
-  colnames(node)[3] <- "name"
-  node <- node[, c(1, 2:3)]
-  
-  tbl_graph(nodes = node, edges = edges, directed = FALSE)
+  pd <- list(nodes = MDS, edges = m)
+  return(pd)
 }
 
 # Plot the entities and its edges
 
 
 # Function to plot the data with ggplot2 if installed.
+#' Plot how similar are the data
+#' @param pd The plot data from `plot_data()` function.
+#' @returns A ggplot object
+#' @importFrom stats cmdscale
+#' @importFrom stats quantile
+#' @export 
+#' @examples 
+#' if (require("org.Hs.eg.db") & require("reactome.db")) {
+#'   # Extract the paths of all genes of org.Hs.eg.db from KEGG
+#'   # (last update in data of June 31st 2011)
+#'   genes.kegg <- as.list(org.Hs.egPATH)
+#'   # Extracts the paths of all genes of org.Hs.eg.db from reactome
+#'   genes.react <- as.list(reactomeEXTID2PATHID)
+#'   
+#'   sim <- mgeneSim(c("81", "18", "10"), genes.react)
+#'   pd <- plot_data(sim, top = 0.25)
+#'   if (requireNamespace("ggplot2", quietly = TRUE)){
+#'     plot_similarity(pd)
+#'   }
+#' }
 plot_similarity <- function(pd) {
-  if (!requireNamespace("tidygraph", quietly = TRUE)) {
-    stop("Please install the ggraph package.")
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    stop("Please install the ggplot2 package.")
   }
-
-  ggraph(graph, layout = "manual", x = x, y = y) +
-    geom_edge_link() + 
-    geom_node_label(aes(label = name)) +
-    theme_graph()
-    
+  .data <- NULL # Trick to avoid check notes
+  p <- ggplot2::ggplot() +
+    ggplot2::geom_segment(data = pd$edges, 
+                          ggplot2::aes(x = .data$x.start, y = .data$y.start, 
+                                       xend = .data$x.end, yend = .data$y.end, 
+                                       linewidth = .data$strength)) +
+    ggplot2::geom_label(data = pd$nodes, 
+                        ggplot2::aes(.data$x, .data$y, label = .data$node), fill = "white") +
+    ggplot2::theme_void()
+  return(p)
 }
